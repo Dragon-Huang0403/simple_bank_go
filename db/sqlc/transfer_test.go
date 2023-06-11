6982870e98dacc55408cb62d0072244b785882a1
Dragon Huang <j0918023423@gmail.com>
2023-06-11 18:20:03 +0800

    test(db): add test for transfers

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,81 @@
+package db
+
+import (
+	"context"
+	"simple_bank/utils"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, fromAccountID int64, toAccountID int64) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: fromAccountID,
+		ToAccountID:   toAccountID,
+		Amount:        utils.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1.ID, account2.ID)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1.ID, account2.ID)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.CreatedAt, transfer2.CreatedAt)
+}
+
+func TestListTransfers(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1.ID, account2.ID)
+	}
+
+	var args [2]ListTransfersParams
+	args[0] = ListTransfersParams{
+		FromAccountID: account1.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+	args[1] = ListTransfersParams{
+		ToAccountID: account2.ID,
+		Limit:       5,
+		Offset:      5,
+	}
+	for _, arg := range args {
+		transfers, err := testQueries.ListTransfers(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, transfers)
+
+		for _, transfer := range transfers {
+			require.NotEmpty(t, transfer)
+		}
+	}
+}
